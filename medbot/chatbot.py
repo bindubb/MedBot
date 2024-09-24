7c4c698db5880d51f3bcb176576bc030a41b5e61
Bindu rao <149154670+bindubb@users.noreply.github.com>
2024-09-25 02:26:05 +0530

    Add files via upload

---
+++ medbot/chatbot.py
@@ -0,0 +1,108 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+from app_test import css, bot_template, user_template
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunk(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handleUserInput(user_question):
+    if st.session_state.uploaded_pdf:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
+
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+    else:
+        st.write(bot_template.replace(
+            "{{MSG}}", "Please upload a pdf file"), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with PDF", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "uploaded_pdf" not in st.session_state:
+        st.session_state.uploaded_pdf = False
+
+    st.header("🏥👨‍⚕️Chat with YAKU bot")
+
+    user_question = st.text_input("Ask a question :")
+    if user_question:
+        print(user_question)
+        handleUserInput(user_question)
+
+    with st.sidebar:
+        st.image("/workspaces/llm-examples/bot.png")
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your 🩺 MED PDFs here and click on 'Process",
+                                    accept_multiple_files=True)
+
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                chunks = get_text_chunk(raw_text)
+                vectorstore = get_vectorstore(chunks)
+
+                conversation_chain = get_conversation_chain(vectorstore)
+
+                st.session_state.conversation = conversation_chain
+                st.session_state.uploaded_pdf = True
+
+
+if __name__ == '__main__':
+    main()
